c75892aae2874e06bb1d0df264d863360c9c4cd9
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-08-18 10:03:19 +0530

    hashtable open addressing and chaning added. stack have added the stack using linkedlist.

--- hashTable.js
+++ hashTable.js
@@ -56,3 +56,197 @@
 // =====================================================================================================
 
 
+// hashtable chaining
+// ----------------------------
+
+// class HashTableChaining {
+//     constructor(size) {
+//         this.table = new Array(size);
+//         this.size = size;
+//     }
+
+//     hash(key) {
+//         let total = 0;
+//         for (let i = 0; i < key.length; i++) {
+//             total += key.charCodeAt(i);
+//         }
+//         return total % this.size;
+//     }
+
+//     set(key, value) {
+//         const index = this.hash(key);
+//         if (!this.table[index]) {
+//             this.table[index] = [];
+//         }
+//         for (let i = 0; i < this.table[index].length; i++) {
+//             if (this.table[index][i][0] === key) {
+//                 this.table[index][i][1] = value;
+//                 return;
+//             }
+//         }
+//         this.table[index].push([key, value]);
+//     }
+
+//     get(key) {
+//         const index = this.hash(key);
+//         const bucket = this.table[index];
+//         if (bucket) {
+//             for (let i = 0; i < bucket.length; i++) {
+//                 if (bucket[i][0] === key) {
+//                     return bucket[i][1];
+//                 }
+//             }
+//         }
+//         return undefined;
+//     }
+
+//     remove(key) {
+//         const index = this.hash(key);
+//         const bucket = this.table[index];
+//         if (bucket) {
+//             this.table[index] = bucket.filter(pair => pair[0] !== key);
+//         }
+//     }
+
+//     display() {
+//         this.table.forEach((val, i) => {
+//             if (val && val.length > 0) {
+//                 console.log(i, val);
+//             }
+//         });
+//     }
+// }
+
+
+
+// =====================================================================================================
+
+// open addressing 
+// -----------------------------
+
+// class HashTableLinearProbing {
+//     constructor(size) {
+//         this.table = new Array(size);
+//         this.size = size;
+//     }
+
+//     hash(key) {
+//         let total = 0;
+//         for (let i = 0; i < key.length; i++) {
+//             total += key.charCodeAt(i);
+//         }
+//         return total % this.size;
+//     }
+
+// set(key,value) {
+//     let index = this.hash(key);
+//     while(this.table[index] && this.table[index] !== key) {
+//         if(this.table[index][0] === key) {
+//             this.table[index][1] = value;
+//             return
+//         }
+//         index = (index + 1) % this.size;
+//     }
+//     this.table[index] = [key,value];
+// }
+
+//     get(key) {
+//         let index = this.hash(key);
+//         while (this.table[index]) {
+//             if (this.table[index][0] === key) {
+//                 return this.table[index][1];
+//             }
+//             index = (index + 1) % this.size;
+//         }
+//         return undefined;
+//     }
+
+//     remove(key) {
+//         let index = this.hash(key);
+//         while (this.table[index]) {
+//             if (this.table[index][0] === key) {
+//                 this.table[index] = null; // use null to mark "deleted"
+//                 return;
+//             }
+//             index = (index + 1) % this.size;
+//         }
+//     }
+
+//     display() {
+//         this.table.forEach((val, i) => {
+//             if (val) {
+//                 console.log(i, val);
+//             }
+//         });
+//     }
+// }
+
+
+
+// =====================================================================================================
+
+// double hahing 
+// --------------------------
+
+// class HashTableDoubleHashing {
+//     constructor(size) {
+//         this.table = new Array(size);
+//         this.size = size;
+//     }
+
+//     hash1(key) {
+//         let total = 0;
+//         for (let i = 0; i < key.length; i++) {
+//             total += key.charCodeAt(i);
+//         }
+//         return total % this.size;
+//     }
+
+//     hash2(key) {
+//         return 7 - (this.hash1(key) % 7);
+//     }
+
+//     set(key, value) {
+//         let index = this.hash1(key);
+//         const step = this.hash2(key);
+
+//         while (this.table[index] && this.table[index][0] !== key) {
+//             index = (index + step) % this.size;
+//         }
+//         this.table[index] = [key, value];
+//     }
+
+//     get(key) {
+//         let index = this.hash1(key);
+//         const step = this.hash2(key);
+
+//         while (this.table[index]) {
+//             if (this.table[index][0] === key) {
+//                 return this.table[index][1];
+//             }
+//             index = (index + step) % this.size;
+//         }
+//         return undefined;
+//     }
+
+//     remove(key) {
+//         let index = this.hash1(key);
+//         const step = this.hash2(key);
+
+//         while (this.table[index]) {
+//             if (this.table[index][0] === key) {
+//                 this.table[index] = null; // mark deleted
+//                 return;
+//             }
+//             index = (index + step) % this.size;
+//         }
+//     }
+
+//     display() {
+//         this.table.forEach((val, i) => {
+//             if (val) {
+//                 console.log(i, val);
+//             }
+//         });
+//     }
+// }
--- stack.js
+++ stack.js
@@ -41,3 +41,138 @@
 // console.log(stack.getSize());
 
 // stack.print();
+
+
+
+
+// sort of stack
+// ---------------------------------
+
+// class Stack {
+//     constructor() {
+//         this.items = [];
+//     }
+
+//     push(element) {
+//         this.items.push(element);
+//     }
+
+//     pop() {
+//         return this.items.pop();
+//     }
+
+//     peek() {
+//         return this.items[this.items.length - 1];
+//     }
+
+//     isEmpty() {
+//         return this.items.length === 0;
+//     }
+
+//     print() {
+//         console.log(this.items);
+//     }
+    
+//     sort() {
+//         const tempStack = [];
+
+//         while (!this.isEmpty()) {
+//             let temp = this.pop();
+
+//             // Move elements from tempStack back to original stack if they are greater than temp
+//             while (tempStack.length > 0 && tempStack[tempStack.length - 1] > temp) {
+//                 this.push(tempStack.pop());
+//             }
+
+//             tempStack.push(temp);
+//         }
+
+//         // Optional: copy back to original stack
+//         while (tempStack.length > 0) {
+//             this.push(tempStack.pop());
+//         }
+//     }
+// }
+
+// // Example usage
+// const stack = new Stack();
+// stack.push(30);
+// stack.push(10);
+// stack.push(50);
+// stack.push(20);
+// stack.print();  // Before sort
+
+// stack.sort();
+// stack.print();  // After sort (ascending order)
+
+
+
+// remove a index value 
+// ---------------------------------
+
+// class Stack {
+//     constructor() {
+//         this.item = [];
+//     }
+
+//     push(element) {
+//         this.item.push(element);
+//     }
+
+//     pop() {
+//         return this.item.pop();
+//     }
+
+//     isEmpty() {
+//         return this.item.length === 0;
+//     }
+
+//     getSize() {
+//         return this.item.length;
+//     }
+
+//     peek() {
+//         if (!this.isEmpty()) {
+//             return this.item[this.item.length - 1];
+//         }
+//         return null;
+//     }
+
+//     print() {
+//         console.log(this.item);
+//     }
+
+//     removeAt(indexToRemove) {
+//         if (indexToRemove < 0 || indexToRemove >= this.item.length) {
+//             console.log("Invalid index");
+//             return;
+//         }
+
+//         const tempStack = [];
+
+//         // Pop items until we reach the index to remove
+//         while (this.item.length - 1 > indexToRemove) {
+//             tempStack.push(this.pop());
+//         }
+
+//         // Remove the target element
+//         this.pop();
+
+//         // Push elements back into original stack
+//         while (tempStack.length > 0) {
+//             this.push(tempStack.pop());
+//         }
+//     }
+// }
+
+// // Example usage
+// const stack = new Stack();
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+// stack.push(40);
+// stack.push(50);
+// stack.print(); // [10, 20, 30, 40, 50]
+
+// stack.removeAt(2); // Remove value at index 2 (value 30)
+// stack.print(); // [10, 20, 40, 50]
